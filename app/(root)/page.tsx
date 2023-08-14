892d7755726dc3eb70455c6b483d9282fc247805
Amarito75 <a.matallah767@gmail.com>
2023-08-14 16:04:00 +0200

    :passport_control: add clerk auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,8 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="p-8">
-      <Button size={"default"} variant={"outline"}>
-        Press
-      </Button>
-    </div>
-  );
-}
+const SetupPage = () => {
+  return <div className="">protected route</div>;
+};
+
+export default SetupPage;
